76b1b30b24252c49e5235879671b4ea06c72db48
Florida-data <fl0r1d4.d4t4@gmail.com>
2024-10-04 13:30:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
-let lenis = new Lenis({
-    duration: 1.2,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10*t))
-})
-function raf(time){
-    lenis.raf(time);
-    requestAnimationFrame(raf);
 
-}
-requestAnimationFrame(raf);
 
 gsap.from("#navbar-titulo",{
     
